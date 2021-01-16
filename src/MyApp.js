9f756975d5a4a15fcde03abd6e97a1ed1eeb13a2
Sophia <49248015+sparrett@users.noreply.github.com>
2021-01-15 19:34:00 -0800

    5 - The submit button and POST calls to add users

--- src/MyApp.js
+++ src/MyApp.js
@@ -45,7 +45,25 @@ function MyApp() {
       }
    }
 
+   async function makePostCall(person){
+      try {
+         const response = await axios.post('http://localhost:5000/users', person);
+         return response;
+      }
+      catch (error) {
+         console.log(error);
+         return false;
+      }
+   }
+
    useEffect(() => { fetchAll().then( result => { if (result) setCharacters(result);});}, [] );
+
+   function updateList(person) { 
+      makePostCall(person).then( result => {
+      if (result)
+         setCharacters([...characters, person] );
+      });
+   }
 
 
    return (
