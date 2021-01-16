63b11a9d6a8b64e8d0c67b90f58a7b41b703477e
Sophia <49248015+sparrett@users.noreply.github.com>
2021-01-15 22:31:41 -0800

    6.4 Async Delete Function added

--- src/MyApp.js
+++ src/MyApp.js
@@ -6,18 +6,36 @@ import axios from 'axios';
 
 function MyApp() {
    const [characters, setCharacters] = useState([]);
+   //     [  variable, setWhatever ]
+   // set charecters =  state 
+   // have to put this around what you want to change 
    
-   function removeOneCharacter (index) {
+   async function removeOneCharacter (index) {
       const updated = characters.filter((character, i) => {
          return i !== index
       });
       setCharacters(updated);
+      var item = characters[index]
+      const id = item['id']
+      console.log(item)
+      console.log(item['id'])
+      // const id = item.get('id')
+      
+      const url = `http://127.0.0.1:5000/users/${id}`
+      console.log(url)
+
+      
+      const response = await axios.delete(url);
+      console.log(response)
+      return response
    }
+
+
    
-   // CHECK THIS FUNCTION 
-   function updateList(person) {
-   	setCharacters([...characters, person]);
-   }
+   // DELETE THIS FUNCTION 
+   // function updateList(person) {
+   // 	setCharacters([...characters, person]);
+   // }
 
    // function DeleteItem(){
    //    // how do I get the id in there??? / how does this replace remove One Charecter and where is 
@@ -45,9 +63,11 @@ function MyApp() {
       }
    }
 
+   useEffect(() => { fetchAll().then( result => { if (result) setCharacters(result);});}, [] );
+
    async function makePostCall(person){
       try {
-         const response = await axios.post('http://localhost:5000/users', person);
+         const response = await axios.post('http://localhost:5000/users', person); // backend value 
          return response;
       }
       catch (error) {
@@ -56,14 +76,27 @@ function MyApp() {
       }
    }
 
-   useEffect(() => { fetchAll().then( result => { if (result) setCharacters(result);});}, [] );
-
    function updateList(person) { 
-      makePostCall(person).then( result => {
+      makePostCall(person).then( result => { // passed into front end 
       if (result.status === 201)
-         setCharacters([...characters, person] );
+         console.log(result)
+          
+         setCharacters([...characters, result.data] );
+         // what is useState and how is the front end updated on the screen? 
+         // 1.) make value 
+            // const array = [1,2,3] 
+         // 2.) change value locally 
+            // characters.push(4) 
+         // 3.) check value 
+            // console.log(characters) = = 1,2,3,4 
+         // 4.) Update it to output ?? 
+            // WILL NOT UPDATE until setCharecters(    charecters <- is manipulayed )
+         // Run npm start to check ! 
       });
    }
+   
+   // This is what this means: result =  makePostCall(person); 
+
 
 
    return (
