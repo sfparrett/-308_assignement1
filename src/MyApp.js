8460f1eb3a4dc33ca8b24dfe0256e9eb3168c11c
Sophia <49248015+sparrett@users.noreply.github.com>
2021-01-15 19:43:05 -0800

    6.1 non changes

--- src/MyApp.js
+++ src/MyApp.js
@@ -60,7 +60,7 @@ function MyApp() {
 
    function updateList(person) { 
       makePostCall(person).then( result => {
-      if (result)
+      if (result.status === 201)
          setCharacters([...characters, person] );
       });
    }
